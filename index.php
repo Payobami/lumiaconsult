f2bf578a4c41d0ac65c1ff824c74755cabbea176
Prudent Yusuf Ayobami (PAY) <prudentprogrammer@gmail.com>
2021-05-29 11:01:51 +0100

    update

---
+++ index.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="EN" lang="EN" dir="ltr">
+<head profile="http://gmpg.org/xfn/11">
+    <title>..::Lumia Mayes::..</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="imagetoolbar" content="no" />
+    <meta name="keywords" content="Lumia Consult, Lumia Mayes" />
+    <meta name="description" content="Lumia Consult and Lumia Mayes Construction Company" />
+    <meta name="copyright" content="http://www.lumiaconsult.com, lumiaconsult.com" />
+    <link rel="stylesheet" href="styles/layout.css" type="text/css" />
+    <!-- Homepage Specific Elements -->
+    <script type="text/javascript" src="scripts/jquery-1.4.1.min.js"></script>
+    <script type="text/javascript" src="scripts/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="scripts/jquery.timers.1.2.js"></script>
+    <script type="text/javascript" src="scripts/jquery.galleryview.2.1.1.min.js"></script>
+    <script type="text/javascript" src="scripts/jquery.galleryview.setup.js"></script>
+    <link rel="icon" href="images/lumia.png" />
+    <!-- End Homepage Specific Elements -->
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-48779906-1', 'lumiaconsult.com');
+        ga('send', 'pageview');
+
+    </script>
+
+
+</head>
+<body id="top">
+<div class="wrapper row1">
+    <div id="header" class="clear">
+
+
+
+
+
+
+
+        <div class="fl_left" style="width:480px; height:100px;">
+            <div id="logo"><img src="images/lumia.png" width="90" height="91"  /></div>
+            <h1 style="float:; margin-top:0px; margin-right:-15px; background:none !important !important"><a href="index.html"><span style="font-size:25px; font-weight:700">Lumia Consult Ltd.</span></a>
+                <br />
+                <p style="margin-left:230px; font-weight:700">And</p>
+                <span style="font-size:22px; background:#510000; color:#fff">Lumia Mayes Construction Company</span></h1>
+
+            <p style="color:#000; margin-left:65px;margin-top:05px; clear:right; font-weight:600; font-style:italic">Architect + Builder + Engineer</p>
+            <p style="color:#000; margin-left:65px;margin-top:05px; clear:right; font-weight:600; font-style:italic"></p>
+        </div>
+
+
+
+
+        <div class="fl_right" style="width:470px">
+
+            <form action="#" method="post" id="sitesearch">
+                <fieldset>
+                    <legend>Site Search</legend>
+                    <input type="text" value="Search Our Website&hellip;" onfocus="this.value=(this.value=='Search Our Website&hellip;')? '' : this.value ;" />
+                    <input type="image" src="images/search.gif" id="search" alt="Search" />
+                </fieldset>
+            </form>
+            <p style="float:right; margin-right:70px"><a href="index.html">A - Z Index</a> | <a href="hse.htm">HSE</a> | <a href="Lumia - mayes construction company limited Latest Profile.pdf" target="new">Company Profile</a></p>
+        </div>
+        <div id="topnav" style="border-radius:0px">
+            <ul>
+                <li class="active"><a href="index.html">Home</a></li>
+                <li><a href="about.html">About Us</a></li>
+                <li><a href="services.html">Our services</a></li>
+                <li><a href="experience.html">Our Experiences</a></li>
+
+                <li><a href="gallery.html">Gallery</a></li>
+                <li><a href="equipment.html">Plant and Equipment</a></li>
+                <li class="last"><a href="contact.html">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper row2">
+    <div id="hpage_featured" class="clear">
+        <!-- ####################################################################################################### -->
+
+        <div id="featured_slide">
+            <ul>
+                <li><img src="images/lumia/owt.jpg" alt="" />
+                    <div class="panel-overlay">
+                        <h2>Welcome to Lumia Mayes</h2>
+                        <p>We are competent and legally capable when it comes to building and contruction works</p>
+                    </div>
+                </li>
+                <li><img src="images/house1.jpg" alt="" />
+
+                </li>
+
+
+
+
+                <li><img src="images/lumia/church_akobo..jpg" alt="" />
+                    <div class="panel-overlay">
+                        <h2>RCCG Auditorium</h2>
+                        <p>Construction of church auditorium at akobo ibadan</p>
+                    </div>
+                </li>
+
+
+                <li><img src="images/20150224_113815.jpg" alt="" />
+                    <div class="panel-overlay">
+                        <h2>
+                            Completed twin duplex at ikorodu lagos
+                        </h2>
+                        <p>Construction of twin duplex at ikorodu lagos</p>
+                    </div>
+                </li>
+
+                <li><img src="images/lumia/NOUN.jpg" alt="" />
+                    <div class="panel-overlay">
+                        <h2>National Open University Abeokuta</h2>
+                        <p>Renovation of National Open University Abeokuta, Ogun State</p>
+                    </div>
+                </li>
+
+
+
+                <li><img src="images/lumia/intercontinental_front.jpg" alt="" />
+                    <div class="panel-overlay">
+                        <h2>Construction of Intercontinental Bank Plc.</h2>
+                        <p>Lumia Mayes Building & Construction Services for
+                            buildings, renovations and refurbishments <a href="gallery.html">Continue Reading &raquo;</a></p>
+                    </div>
+                </li>
+                <li><img src="images/house.jpg" alt="" />
+                    <div class="panel-overlay">
+                        <h2>Building Construction Services</h2>
+                        <p>With vast industry experience, we have been able to render best-in-class Building Construction Services to the clients. <a href="about.html">Continue Reading &raquo;</a></p>
+                    </div>
+
+                </li>
+
+
+                <li><img src="images/lumia/agungba_oka2.jpg" alt="" />
+                    <div class="panel-overlay">
+                        <h2>National Highway Construction</h2>
+                        <p style="line-height:15px; margin-top:-5px">We are a reputed firm known for offering services for National Highway Construction. Our staff makes use of technically advanced equipment for the construction of highways. <a href="gallery.html">Continue Reading &raquo;</a></p>
+                    </div>
+                </li>
+
+
+                <li><img src="images/20150705_073500.jpg" alt="" />
+                    <div class="panel-overlay">
+                        <h2>Completed bungalow at Akobo Ibadan</h2>
+                        <p style="line-height:15px; margin-top:-5px">We are a reputed firm known for able to render best-in-class Building Construction Services to the clients. <a href="gallery.html">Continue Reading &raquo;</a></p>
+                    </div>
+                </li>
+
+
+            </ul>
+        </div>
+
+
+        <div class="intro clear">
+            <div class="welcome clear"><img class="imgl" src="images/house.jpg" height="100" width="125" alt="" style="border:none" />
+                <div class="fl_right">
+                    <h2>Welcome to Our Construction Company</h2>
+                    <p style="color:#666">Lumia - Mayes Construction Company limited was incorporated as a private limited liability company to offer strictly professional and specialized services in building, Civil and Telecommunication engineering among others.</p>
+                </div>
+            </div>
+            <div class="popular">
+                <h2>Most Popular Links</h2>
+                <ul class="clear">
+                    <li><a href="cooperate2.html">&raquo; Cooperate Tent</a></li>
+                    <li><a href="gallery.html">&raquo; Gallery</a></li>
+                    <li><a href="about.html">&raquo; About Us</a></li>
+                    <li><a href="about.html">&raquo; Our Research</a></li>
+                    <li><a href="about.html">&raquo; Our Client</a></li>
+                    <li><a href="services.html">&raquo; Our Service</a></li>
+                    <li><a href="ostructure2.html">&raquo; Organization Structure</a></li>
+
+                    <li><a href="hse.htm">&raquo; Policies on Safety</a></li>
+                    <li><a href="keystaff.html">&raquo; Organogram</a></li>
+                    <li><a href="Lumia - mayes construction company limited Latest Profile.pdf">&raquo; Downlaod Our Company Profile</a></li>
+                    <li><a href="experience.html">&raquo; Work experience by the company &emsp;and directors</a></li>
+                    <li><a href="keystaff.html">&raquo; Key Staff</a></li>
+
+                </ul>
+            </div>
+        </div>
+
+
+
+        <!-- ###### --><!-- ####################################################################################################### -->
+    </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper row3">
+    <div id="container" class="clear">
+        <!-- ####################################################################################################### -->
+        <div id="homepage" class="clear">
+            <!-- ###### -->
+            <div id="content">
+                <div id="top_featured" class="clear">
+                    <ul class="clear">
+                        <li>
+                            <h2>About Lumia-Mayes</h2>
+                            <img src="images/ac.jpg" width="300" height="100" alt="" />
+                            <p style="color:#666">Lumia - Mayes Construction Company limited was incorporated as a private limited liability company to offer strictly professional and specialized services in building,</p>
+                            <p class="readmore"><a href="about.html">Continue Reading &raquo;</a></p>
+                        </li>
+                        <li class="last">
+                            <h2>Organisation Structure</h2>
+                            <img src="images/bim-diagram[0].jpg" width="300" height="100" alt="" />
+                            <p style="color:#666">The Organisation structure of Lumia - mayes construction company limited consists of a Technical Department headed by the Technical Director and an Administration..</p>
+                            <p class="readmore"><a href="ostructure2.html">Continue Reading &raquo;</a></p>
+                        </li>
+                    </ul>
+                </div>
+
+
+
+                <div id="top_featured" class="clear">
+                    <ul class="clear">
+                        <li>
+                            <h2 style=" font-size:14px; font-weight:500">Work experience by the company and directors </h2>
+                            <img src="images/ad.jpg" width="300" height="100" alt="" />
+                            <p style="color:#666">Throughout this time Lumia Mayes has deployed progressive construction methods to ensure that innovation and quality are prioritized for the benefit of clients.</p>
+                            <p class="readmore"><a href="experience.html">Continue Reading &raquo;</a></p>
+                        </li>
+                        <li class="last">
+                            <h2>Architect and Builder</h2>
+                            <img src="images/archi.jpg" width="300" height="100" alt="" />
+                            <p style="color:#666">With a highly-experienced and professional management team, we deliver first-class projects across the nation for both public and private sector clients..
+                                <br />
+                            </p>
+                            <p class="readmore"><a href="about.html">Continue Reading &raquo;</a></p>
+                        </li>
+                    </ul>
+                </div>
+
+
+
+
+
+
+            </div>
+            <!-- ###### -->
+            <div id="column">
+
+                <div class="holder last">
+                    <h2>Directors</h2>
+                    <ul class="staffmembers">
+                        <li class="clear" style="height:212px"><a href="ceo2.html"><img class="imgl" src="images/lumia/ceo_1.jpg" width="120" alt="" /></a>
+                            <div class="fl_left">
+                                <p><strong>Ayegbusi O.T.</strong></p>
+                                <p>C.E.O</p>
+                                <p>
+                                    <a href="ceo2.html">View Full Profile &raquo;</a></p>
+                            </div>
+
+
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div id="column">
+
+                <div class="holder last">
+                    <h2 style="text-align:center">Construction of Unipole, Portrait, Crossway, Bulletin and Gantry.</h2>
+                    <ul class="staffmembers">
+                        <li class="clear"><a href="ceo2.html"><img class="imgl" src="images/Unipole_lagos80.jpg" alt="" /></a>
+                            <div class="fl_left">
+                                <p><strong>Unipole at:</strong></p>
+                                <p>Third Mid-land Bridge
+                                    <br />Lagos State</p>
+                                <p><a href="gallery.html">View Our Gallery&raquo;</a></p>
+                            </div>
+                        </li>
+                        <li class="clear"><img class="imgl" src="images/moro_mast.jpg" height="80" width="80" alt="" />
+                            <div class="fl_left">
+                                <p><strong>Mast at:</strong></p>
+                                <p>Moro in Osun State</p>
+                                <p><a href="gallery.html">View Our Gallery&raquo;</a></p>
+                            </div>
+                        </li>
+                        <li class="clear"><img class="imgl" src="images/Geotechni_amzor.jpg" height="80" width="80" alt="" />
+                            <div class="fl_left">
+                                <p><strong>Geotechnic at:</strong></p>
+                                <p>Emzor</p>
+                                <p><a href="gallery.html">View Our Gallery&raquo;</a></p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <!-- ###### -->
+        </div>
+        <!-- ####################################################################################################### -->
+    </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper row4">
+    <div id="footer" class="clear">
+        <!-- ####################################################################################################### -->
+        <div class="footbox">
+            <h2>Quick Links</h2>
+            <ul>
+                <li><a href="index.html">&raquo; Homepage</a></li>
+                <li><a href="contact.html">&raquo; Contact Us</a></li>
+                <li><a href="projectDM.html">&raquo; Sitemap</a></li>
+                <li><a href="about.html">&raquo; Privacy Policy</a></li>
+                <li><a href="about.html">&raquo; Terms of Use</a></li>
+                <li><a href="about.html">&raquo; Copyright Information</a></li>
+                <li><a href="about.html">&raquo; Website Matters</a></li>
+            </ul>
+        </div>
+        <div class="footbox" style="width:280px">
+            <h2>How To Find Us</h2>
+            <address>
+                Suite 38, Harmonic Plaza,Mike Aighbe,Off Obafemi Awolowo way, Jabi, Abuja<br />
+                Suite 4, Al-Barakan Plaza, Favous Bodija Ibadan
+                <br />
+                6, Amore Off Toyin Street, Ikeja Lagos.
+            </address>
+            <address>
+
+                Tel: 08037136696
+                <br style="margin-bottom:5px !important"/>
+                Tel: 08072397483<br />
+                Email: <a href="mail:mayes777@yahoo.com">mayes777@yahoo.com</a>
+            </address>
+        </div>
+        <div class="fl_right">
+            <div id="social">
+                <h2>Connect With Us</h2>
+                <ul>
+                    <li><a href="http://facebook.com"><img src="images/social/facebook.gif" alt="" /></a></li>
+                    <li><a href="http://twitter.com"><img src="images/social/twitter.gif" alt="" /></a></li>
+                    <li><a href="http://flickr.com"><img src="images/social/flickr.gif" alt="" /></a></li>
+                    <li><a href="http://youtube.com"><img src="images/social/youtube.gif" alt="" /></a></li>
+                    <li class="last"><a href="http://rss.com"><img src="images/social/rss.gif" alt="" /></a></li>
+                </ul>
+            </div>
+            <div id="newsletter" class="clear">
+                <form action="#" method="post">
+                    <fieldset>
+                        <legend>Subscribe To Our Newsletter:</legend>
+                        <input type="text" value="Enter Email Here&hellip;" onfocus="this.value=(this.value=='Enter Email Here&hellip;')? '' : this.value ;" />
+                        <input type="button" id="subscribe" value="Submit" />
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+        <!-- ####################################################################################################### -->
+    </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper">
+    <div id="copyright" class="clear">
+        <p class="fl_left">Copyright &copy; 2013 - All Rights Reserved - <a href="index.html">Domain Name</a></p>
+        <p class="fl_right">Template by <a href="index.html" title="Free Website Templates">Beyad Technology</a></p>
+    </div>
+</div>
+</body>
+</html>
